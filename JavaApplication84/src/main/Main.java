6d499e136c78db1d40019727da03f0d8f7181c04
AgusH <agustin.herrara2005@gmail.com>
2026-06-09 18:21:14 -0300

    main2

---
+++ JavaApplication84/src/main/Main.java
@@ -0,0 +1,17 @@
+
+package main;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
